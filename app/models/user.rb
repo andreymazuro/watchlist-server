33fb59bc918d5406422d78dc94360c7467516ac1
Andrey Mazuro <andrey.mazuro@icloud.com>
2017-01-10 04:54:13 +0300

    vsdf

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :name,:email, uniqueness: true
-  has_many :users_to_movies
+  has_many :users_to_movies, dependent: :destroy
   has_many :movies, through: :users_to_movies
 end
