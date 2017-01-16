479bcf32d12008fa5dbdec47456f10028260063c
Andrey Mazuro <andrey.mazuro@icloud.com>
2017-01-16 04:06:15 +0300

    we

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,1 @@
-require 'redis'
-redis = Redis.new (
-    :host => 'redis-12162.c10.us-east-1-3.ec2.cloud.redislabs.com',
-    :port => 12162,
-    :password => '5018120aA')
+redis = Redis.new (:host => 'redis-12162.c10.us-east-1-3.ec2.cloud.redislabs.com', :port => 12162, :password => '5018120aA')
