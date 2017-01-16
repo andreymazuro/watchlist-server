dbd58e338ca381bbc2f06ae888a275e2d7a1404c
Andrey Mazuro <andrey.mazuro@icloud.com>
2017-01-16 04:11:36 +0300

    we

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,1 @@
-redis = Redis.new (:host => 'redis-12162.c10.us-east-1-3.ec2.cloud.redislabs.com', :port => 12162, :password => '5018120aA')
+$redis = Redis::Namespace.new("my_app", :redis => Redis.new(:host => "redis-12162.c10.us-east-1-3.ec2.cloud.redislabs.com", :port => 12162, :password => '5018120aA'))
